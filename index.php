833ab58202280a28eb977badf7a6a346295925de
Jesus Rodriguez <z0e0u0s@gmail.com>
2012-11-14 21:12:27 -0500

    test commit

--- index.php
+++ index.php
@@ -24,6 +24,8 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+// Just a test 
+ 
 require_once 'app/bootstrap.php';
 
 /* Store or website code */
